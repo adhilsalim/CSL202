be20e2d0634155966c811317988776c869b1cbdb
Adhil Salim <bizadhil@gmail.com>
2023-08-17 09:04:37 +0530

    add comments function

--- FUNCTIONS/FUNCTION-1/testbench.v
+++ FUNCTIONS/FUNCTION-1/testbench.v
@@ -7,15 +7,16 @@ module funtest; // Test bench module
   fun f1(A1, B1, C1, F); // Instantiate the function module
 
   initial begin
-    A1 = 1'b0; B1 = 1'b0; C1 = 1'b0; // Initialize inputs A1, B1, and C1 to 0
-    #1 A1 = 1'b0; B1 = 1'b0; C1 = 1'b1; // Change inputs after 1 time unit
-    #1 A1 = 1'b0; B1 = 1'b1; C1 = 1'b0; // Change inputs after 1 more time unit
-    #1 A1 = 1'b0; B1 = 1'b1; C1 = 1'b1; // Change inputs after 1 more time unit
-    #1 A1 = 1'b1; B1 = 1'b0; C1 = 1'b0; // Change inputs after 1 more time unit
-    #1 A1 = 1'b1; B1 = 1'b0; C1 = 1'b1; // Change inputs after 1 more time unit
-    #1 A1 = 1'b1; B1 = 1'b1; C1 = 1'b0; // Change inputs after 1 more time unit
-    #1 A1 = 1'b1; B1 = 1'b1; C1 = 1'b1; // Change inputs after 1 more time unit
-    #1 A1 = 1'b1; B1 = 1'b0; C1 = 1'b0; // Change inputs after 1 more time unit
-    #1 A1 = 1'b1; B1 = 1'b0; C1 = 1'b0; // Change inputs after 1 more time unit
+    A1 = 1'b0; B1 = 1'b0; C1 = 1'b0; // Initialize inputs A1, B1, and C1 to 0 000
+
+    #1 A1 = 1'b0; B1 = 1'b0; C1 = 1'b1; // Change inputs after 1 time unit 001
+    #1 A1 = 1'b0; B1 = 1'b1; C1 = 1'b0; // Change inputs after 1 more time unit 010
+    #1 A1 = 1'b0; B1 = 1'b1; C1 = 1'b1; // Change inputs after 1 more time unit 011
+    #1 A1 = 1'b1; B1 = 1'b0; C1 = 1'b0; // Change inputs after 1 more time unit 100
+    #1 A1 = 1'b1; B1 = 1'b0; C1 = 1'b1; // Change inputs after 1 more time unit 101
+    #1 A1 = 1'b1; B1 = 1'b1; C1 = 1'b0; // Change inputs after 1 more time unit 110
+    #1 A1 = 1'b1; B1 = 1'b1; C1 = 1'b1; // Change inputs after 1 more time unit 111
+
+    #1 A1 = 1'b1; B1 = 1'b0; C1 = 1'b0; // to get the last output in waveform, values doesn't matter
   end
 endmodule
